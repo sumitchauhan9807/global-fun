bccb507c57af0bc3112dca64ffab3bece02cc17f
Sumit chauhan <sumitchauhan9807666@gmail.com>
2025-01-20 18:15:48 +0530

    wip

--- src/mediasoup/createWebRtcTransport.ts
+++ src/mediasoup/createWebRtcTransport.ts
@@ -1,31 +1,34 @@
-const createWebRtcTransport:any = (router:any)=>new Promise(async(resolve, reject)=>{
-  const transport = await router.createWebRtcTransport({
+const createWebRtcTransport: any = (router: any) =>
+  new Promise(async (resolve, reject) => {
+    const transport = await router.createWebRtcTransport({
       enableUdp: true,
       enableTcp: true, //always use UDP unless we can't
       preferUdp: true,
       listenInfos: [
-          {
-              protocol: 'udp',
-              ip: '127.0.0.1',
-              // ip: '0.0.0.0',
-              // announcedIp: '127.0.0.1',
-          },
-          {
-              protocol: 'tcp',
-              ip: '127.0.0.1',
-              // ip: '0.0.0.0',
-              // announcedIp: '127.0.0.1',
-          }
-      ]
-  })
-  // console.log(transport)
-  const clientTransportParams = {
+        {
+          protocol: "udp",
+          ip: "65.109.126.185",
+          // port:443
+          // ip: '0.0.0.0',
+          // announcedIp: '127.0.0.1',
+        },
+        {
+          protocol: "tcp",
+          ip: "65.109.126.185",
+          // port:443
+          // ip: '0.0.0.0',
+          // announcedIp: '127.0.0.1',
+        },
+      ],
+    });
+    // console.log(transport)
+    const clientTransportParams = {
       id: transport.id,
       iceParameters: transport.iceParameters,
       iceCandidates: transport.iceCandidates,
       dtlsParameters: transport.dtlsParameters,
-  }
-  resolve({transport,clientTransportParams})
-})
+    };
+    resolve({ transport, clientTransportParams });
+  });
 
-export default createWebRtcTransport
+export default createWebRtcTransport;
