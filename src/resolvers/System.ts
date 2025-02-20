9deffc020c8d37e58f1b0678cae206df15d05ae9
Sumit chauhan <sumitchauhan9807666@gmail.com>
2025-02-20 16:43:07 +0530

    asd

--- src/resolvers/System.ts
+++ src/resolvers/System.ts
@@ -45,5 +45,25 @@ export class SystemResolver {
     }
   }
 
+  @Query(() => Boolean)
+  async system_check_session_active(
+    @Arg("sessionId") sessionId : number,
+    @Arg("authToken") authToken : string
+  ) {
+    // console.log(username,"modelId from ststsres")
+    try{
+      if(authToken != CROSS_SERVER_AUTH_TOKEN) throw Error('Auth Failed')
+      let findSession = await LiveSession.findOne({
+        where : { id:sessionId}
+      })
+      if(!findSession) return false
+      if(findSession?.status == LIVE_SESSION_STATUS.ENDED) return false
+      if(findSession?.status == LIVE_SESSION_STATUS.IN_PROGRESS) return true
+    }catch(e) {
+      console.log(e)
+      return e
+    }
+  }
+
   
 }
